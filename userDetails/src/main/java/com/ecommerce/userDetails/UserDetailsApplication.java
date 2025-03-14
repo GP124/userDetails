a19b23e3d62e227929237ed913dfd340b11a7d7a
guruprakash124 <guru.prakash12499@gmail.com>
2025-03-14 17:59:13 +0530

    Create entity class

--- userDetails/src/main/java/com/ecommerce/userDetails/UserDetailsApplication.java
+++ userDetails/src/main/java/com/ecommerce/userDetails/UserDetailsApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.userDetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.javaguides.emsbackend.entity")
 public class UserDetailsApplication {
 
 	public static void main(String[] args) {
--- userDetails/src/main/java/com/ecommerce/userDetails/entity/UserEntity.java
+++ userDetails/src/main/java/com/ecommerce/userDetails/entity/UserEntity.java
@@ -1,4 +1,27 @@
 package com.ecommerce.userDetails.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "userLoginDetails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "userName", unique = true,nullable = false)
+    private String userName;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Column(name = "email",nullable = false)
+    private String email;
 }
