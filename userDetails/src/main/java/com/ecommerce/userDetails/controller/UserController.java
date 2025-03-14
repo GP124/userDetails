c1dda62bf97064dd739725bcb1a3811462ef090b
guruprakash124 <guru.prakash12499@gmail.com>
2025-03-14 18:30:19 +0530

    Create controller  class

--- userDetails/src/main/java/com/ecommerce/userDetails/controller/UserController.java
+++ userDetails/src/main/java/com/ecommerce/userDetails/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.ecommerce.userDetails.controller;
 
+import com.ecommerce.userDetails.dto.UserDto;
+import com.ecommerce.userDetails.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/userLoginDetails")
 public class UserController {
+
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto savedUser = userService.createUser(userDto);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
 }
